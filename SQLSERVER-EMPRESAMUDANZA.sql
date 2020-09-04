6fedf0e88f76db126d10ed0e9b8c70f08340a740
Ricardo Lugo <56897905+RYNEEER@users.noreply.github.com>
2020-09-03 20:08:06 -0500

    Add files via upload

---
+++ SQLSERVER-EMPRESAMUDANZA.sql
@@ -0,0 +1,41 @@
+insert into empresamudanza values
+(1,'MantaMudanzas',052498876, 'Flavio Reyeves av. principal');
+--------------------------------------------------------------
+insert into servicio values
+(1, 'Fletes', '10:00'),
+(2, 'Mudanza express', '10:00');
+--------------------------------------------------------------
+insert into vehiculo values
+(1, '2 metros', 'camion', 'tamaño pequeño'),
+
+(2, '5 metros', 'camion', 'tamaño mediano'),
+
+(3, '10 metros', 'camion', 'tamaño grande');
+
+
+insert into solicitud values 
+(1, '10/10/2020', 'calle 12 av 13', 'cuba parroquia eloy alfaro', '10:00','150',5, 1,1,1,1), 
+(2, '25/10/2020', 'calle 14 av 12', 'flavio reyes', '10:00','200',5, 2,2,2,2),
+(3, '30/10/2020', 'calle 10 av 24', 'centro', '10:00','200',5, 3,3,3,3);
+-------------------------------------------------------------
+
+insert into provincia values
+(1, 'Manabi'),
+(2, 'Esmeralda'),
+(3, 'Loja'),
+(4, 'Guayaquil');
+-------------------------------------------------------------
+
+insert into cliente values
+(1, 'Ricardo Lugo','calle 12 av 13',0961019690),
+(2, 'Natalia Macias','calle 14 av 12',0941787114),
+(3, 'Vannesa Macias','calle 10 av 24',0961019690);
+
+-------------------------------------------------------------
+insert into empleado values
+(1, 'Jhon Tejena','calle 09 av 14', '0874641515','Transportista',600),
+(2, 'Ruben Avila','calle 07 av 24','0775641515','Transportista',700),
+(3, 'Deiby Vera','calle 09 av 14','0792641515','Transportista',900);
+-------------------------------------------------------------
+
+
